dfd4e2724832cfedc8b0d05680fed8eb06293111
Marjoriecus <101658277+Marjoriecus@users.noreply.github.com>
2025-06-05 22:14:41 +0000

    final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,50 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronoun = ["The", "Our", "The"];
+  let adj = ["great", "big", "last"];
+  let noun = ["jogger", "racoon", "of"];
+  let extension = [".net", ".com", ".us", ".edu"];
+
+  for (let index0 of pronoun){
+    for (let index1 of adj ) {
+      for (let index2 of noun){
+        for (let index3 of extension)
+
+          document.querySelector("#domain").innerHTML += "<h3>" + index0 + index1 + index2 + index3 + "</h3>"
+
+      }
+    }
+  }
+
+  
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
